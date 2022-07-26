e216c2f24706b6e815cdd32b4e9356a3fed41879
王俊波 <junbo.wang@chinaentropy.com>
2022-07-26 11:18:22 +0800

    feat: 初始化123

--- swagger-service/src/main/java/com/cloud/wjb/controller/TestController.java
+++ swagger-service/src/main/java/com/cloud/wjb/controller/TestController.java
@@ -1,8 +1,9 @@
 package com.cloud.wjb.controller;
 
+import com.cloud.wjb.common.entity.R;
+import com.cloud.wjb.domin.vo.AlertConfigPageVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
