1d68084b3152562dcd9551016c8428a3efdacdd1
王俊波 <junbo.wang@chinaentropy.com>
2022-07-26 15:12:43 +0800

    feat: swagger common

---
+++ wjb-modules/system-service/src/main/java/com/cloud/wjb/system/SystemServiceApplication.java
@@ -0,0 +1,19 @@
+package com.cloud.wjb.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈system-service启动类〉
+ *
+ * @author wjb
+ * @create 2022/7/26
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class SystemServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemServiceApplication.class, args);
+    }
+}
