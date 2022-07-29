382d2e4a09d45aea7ba87296ab3b1721a46a5611
王俊波 <junbo.wang@chinaentropy.com>
2022-07-29 09:08:28 +0800

    feat: netty

---
+++ wjb-modules/netty-servvice/netty-server/src/main/java/com/cloud/wjb/netty/server/NettyServerApplication.java
@@ -0,0 +1,19 @@
+package com.cloud.wjb.netty.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈system-service启动类〉
+ *
+ * @author wjb
+ * @create 2022/7/26
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class NettyServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NettyServerApplication.class, args);
+    }
+}
