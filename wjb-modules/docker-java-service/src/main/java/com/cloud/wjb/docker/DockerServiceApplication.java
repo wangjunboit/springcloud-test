19a904ec2f98f28bdbb9b4df7f7cc6599618106b
王俊波 <junbo.wang@chinaentropy.com>
2022-08-01 11:30:20 +0800

    fix: 新增docker-java模块

---
+++ wjb-modules/docker-java-service/src/main/java/com/cloud/wjb/docker/DockerServiceApplication.java
@@ -0,0 +1,19 @@
+package com.cloud.wjb.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈system-service启动类〉
+ *
+ * @author wjb
+ * @create 2022/7/26
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class DockerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerServiceApplication.class, args);
+    }
+}
---
+++ wjb-modules/docker-java-service/src/main/java/com/cloud/wjb/docker/controller/DockerController.java
@@ -0,0 +1,19 @@
+package com.cloud.wjb.docker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author wjb
+ * @create 2022/8/1
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping("/docker")
+public class DockerController {
+
+
+}
