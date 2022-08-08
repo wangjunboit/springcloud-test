5236baff7906e22b2c3507ab9fdfa0376915a833
王俊波 <junbo.wang@chinaentropy.com>
2022-08-08 14:38:50 +0800

    fix: 新增common-redis模块

---
+++ wjb-modules/docker-java-service/src/test/java/Test.java
@@ -0,0 +1,21 @@
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author wjb
+ * @create 2022/8/4
+ * @since 1.0.0
+ */
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void ifTesting() {
+        int a = 4;
+        if (a > 1) {
+            System.out.println(1);
+        } else if (a > 2) {
+            System.out.println(2);
+        }
+    }
+
+}
--- wjb-modules/system-service/src/main/java/com/cloud/wjb/system/controller/TestController.java
+++ wjb-modules/system-service/src/main/java/com/cloud/wjb/system/controller/TestController.java
@@ -3,6 +3,8 @@ package com.cloud.wjb.system.controller;
 import com.cloud.wjb.common.entity.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "swagger接口管理")
 @RestController
 public class TestController {
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/test")
     @ApiOperation("测试接口")
     public R test(@RequestParam("id") Integer id) {
+        redisTemplate.opsForValue().set("ID", id);
         return R.ok(id);
     }
 }
